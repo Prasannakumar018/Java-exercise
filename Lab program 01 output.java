53633b5f04962de724b0341a5272f362ebf2c9bd
Prasannakumar018 <89631985+Prasannakumar018@users.noreply.github.com>
2021-09-05 22:02:09 +0530

    Create Lab program 01 output.java

---
+++ Lab program 01 output.java
@@ -0,0 +1,24 @@
+
+	 Enter consumer number 2000
+
+	 Enter Type of connection (D for Domestic or C for Commercial) D
+
+	 Enter consumer name kumar
+
+	 Enter previous month reading 200
+
+	 Enter current month reading 250
+
+	 Consumer number = 2000
+
+	 Consumer name = kumar
+
+	 type of connection = DOMESTIC 
+
+	 Current Month Reading = 250.0
+
+	 Previous Month Reading = 200.0
+
+	 Total units = 50.0
+
+	 Total bill = RS 50.0
