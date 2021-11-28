d70e04d140392d2179a5831d7b05ca2bcb030dc5
Prasannakumar018 <89631985+Prasannakumar018@users.noreply.github.com>
2021-11-28 21:01:30 +0530

    Create Practice program 7 output.java

---
+++ Practice program 7 output.java
@@ -0,0 +1,34 @@
+STRING MANIPULATION 
+************************ 
+1. Append at end 2.Insert at particular index 3.Search 
+4. List string that starting with letter 
+5. Size 6.Remove 7.Sort 8.Display 
+Enter the choice: 1 
+Enter the string: FIRST 
+Enter 0 to break and 1 to continue : 1 
+STRING MANIPULATION 
+************************ 
+1. Append at end 2.Insert at particular index 3.Search 
+4. List string that starting with letter 
+5. Size 6.Remove 7.Sort 8.Display 
+Enter the choice: 1 
+Enter the string : LAST 
+Enter 0 to break and 1 to continue : 1 
+STRING MANIPULATION 
+************************ 
+1. Append at end 2.Insert at particular index 3.Search 
+4. List string that starting with letter 
+5. Size 6.Remove 7.Sort 8.Display 
+Enter the choice : 8 
+The array list has following elements:[FIRST, LAST] 
+Enter 0 to break and 1 to continue : 1 
+STRING MANIPULATION 
+************************ 
+1. Append at end 2.Insert at particular index 3.Search 
+4. List string that starting with letter 
+5. Size 6.Remove 7.Sort 8.Display 
+Enter the choice : 2 
+Enter the string: SECOND 
+Specify the index/position to insert : 1 
+The array list has following elements:[SECOND, FIRST, LAST] 
+Enter 0 to break and 1 to continue: 1
